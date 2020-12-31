af2368832bd59041d916708411dbaa60c1407be1
Raúl Caro Pastorino <dev@fryntiz.es>
2020-12-31 02:20:39 +0100

    Reserve space to slide

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,15 +4,28 @@
     <navbar></navbar>
     
     <ion-content>
-      <ion-row>
-        <ion-col>
-          Slide
-        </ion-col>
-      </ion-row>
-
-      <ion-row>
-        <ion-col>
+      <ion-row class="center">
+        <ion-col 
+          size-sm="12"
+          offset-sm="0"
+          size-lg="10" 
+          offset-lg="1" 
+          size-xl="8" 
+          offset-xl="2" 
+          style="background-color:yellow;">
           3 bloques con imágenes: Vídeos en youtube, iniciarse en linux, contribuir
+
+          <ion-slides pager="true" :options="slideOpts" scrollbar="true">
+            <ion-slide>
+              <h1>Slide 1</h1>
+            </ion-slide>
+            <ion-slide>
+              <h1>Slide 2</h1>
+            </ion-slide>
+            <ion-slide>
+              <h1>Slide 3</h1>
+            </ion-slide>
+          </ion-slides>
         </ion-col>
       </ion-row>
 
@@ -34,7 +47,7 @@
 </template>
 
 <script lang="ts">
-import { IonContent, IonPage} from '@ionic/vue';
+import { IonSlides, IonSlide, IonContent, IonPage} from '@ionic/vue';
 import { defineComponent } from 'vue';
 import { add } from 'ionicons/icons';
 import { useRouter } from 'vue-router';
@@ -48,12 +61,21 @@ export default defineComponent({
     IonContent,
     IonPage,
     MainMenu,
-    Navbar
+    Navbar,
+    IonSlides, 
+    IonSlide
   },
   setup() {
+    // Optional parameters to pass to the swiper instance. See http://idangero.us/swiper/api/ for valid options.
+    const slideOpts = {
+      initialSlide: 1,
+      speed: 400
+    };
+
     return {
       router: useRouter(),
-      add
+      add,
+      slideOpts
     }
   }
 });
@@ -86,5 +108,9 @@ export default defineComponent({
 
 #container a {
   text-decoration: none;
+}
+
+.center {
+  text-align: center;
 }
 </style>
