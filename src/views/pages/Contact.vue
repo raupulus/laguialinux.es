cb5610984e0a9e2ce01fc6c070ce9bf843aa5782
Raúl Caro Pastorino <dev@fryntiz.es>
2021-04-30 22:02:45 +0200

    Add basic contact page

---
+++ src/views/pages/Contact.vue
@@ -0,0 +1,45 @@
+<template>
+    <ion-page>
+      <ion-content color="dark">
+        <ion-row class="center">
+          <ion-col
+            size-sm="12"
+            offset-sm="0"
+            size-lg="10"
+            offset-lg="1"
+            size-xl="8"
+            offset-xl="2">
+            Página de contacto
+          </ion-col>
+        </ion-row>
+        
+      </ion-content>
+    </ion-page>  
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { 
+  IonContent,
+  IonCol,
+  IonRow,
+  IonPage
+} from '@ionic/vue';
+
+export default defineComponent({
+  name: 'Contact',
+  components: {
+    IonContent,
+    IonCol,
+    IonRow,
+    IonPage
+  },
+  props: ['forceSlug'],
+});
+</script>
+
+<style scoped>
+.center {
+  text-align: center;
+}
+</style>
