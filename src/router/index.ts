62a914172d68f134145ebb07d6b27d84b251bb22
Raúl Caro Pastorino <dev@fryntiz.es>
2021-04-30 22:02:30 +0200

    Add routes with fixed parameters

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,34 +1,68 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
 import Home from '../views/pages/Home.vue';
-//import GenericPageListElements from '../views/pages/GenericPageListElements.vue';
-import GenericPageShowContent from '../views/pages/GenericPageShowContent.vue';
-
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    redirect: '/home'
+    path: '/home',
+    redirect: '/'
   },
   {
-    path: '/home',
+    path: '/',
     name: 'Home',
+    props: {forceSlug: 'home'},
     component: Home
   },
   {
-    path: '/section/:slug',
+    path: '/search',
+    name: 'Search',
+    props: {forceSlug: 'search'},
+    component: () => import('@/views/pages/Search.vue')
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    props: {forceSlug: 'contact'},
+    component: () => import('@/views/pages/Contact.vue')
+  },
+  {
+    path: '/about',
+    name: 'About',
+    props: {forceSlug: 'about'},
+    component: () => import('@/views/pages/GenericPageListElements.vue')
+  },
+  {
+    path: '/posts',
+    name: 'Posts',
+    props: {forceSlug: 'posts'},
+    component: () => import('@/views/pages/GenericPageListElements.vue')
+  },
+  {
+    path: '/events',
+    name: 'Events',
+    props: {forceSlug: 'events'},
+    component: () => import('@/views/pages/GenericPageListElements.vue')
+  },
+  {
+    path: '/apps',
+    name: 'Apps',
+    props: {forceSlug: 'apps'},
+    component: () => import('@/views/pages/GenericPageListElements.vue')
+  },
+  {
+    path: '/:slug',
+    name: 'content',
+    component: () => import('@/views/pages/GenericPageShowContent.vue')
+  },
+  {
+    path: '/:subsection/:slug',
     name: 'section',
     component: () => import('@/views/pages/GenericPageListElements.vue')
   },
   {
-    path: '/section/:subsection/:slug',
+    path: '/:section/:subsection/:slug',
     name: 'subsection',
     component: () => import('@/views/pages/GenericPageListElements.vue')
-  },
-  {
-    path: '/content/:slug',
-    name: 'content',
-    component: GenericPageShowContent
   },
 ]
 
